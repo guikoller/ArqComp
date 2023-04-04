12030666bd8a0f7702b1bf14862b89436311986a
guikoller <g_koller@hotmail.com>
2023-04-03 21:25:45 -0300

    fix: ula com entrada para escolher operação e operações de comparação

--- ULA/ula.vhd
+++ ULA/ula.vhd
@@ -1,27 +1,49 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity ula is
-    Port ( 
-	   data_in_A : in  unsigned (15 downto 0);
-           data_in_B : in  unsigned (15 downto 0);
-           result_out : out  unsigned (15 downto 0);
-           flag_out : out  std_logic
-   );
-end ula;
+ENTITY ULA IS
+    PORT (
+        data_in_A : IN unsigned(15 DOWNTO 0);
+        data_in_B : IN unsigned(15 DOWNTO 0);
+        op : IN unsigned(1 DOWNTO 0);
+        result_out : OUT unsigned(15 DOWNTO 0);
+        zero_out : OUT STD_LOGIC
+    );
+END ULA;
 
-architecture a_ula of ula is
-begin
-    -- Soma
-    result_out <= data_in_A + data_in_B;
+ARCHITECTURE A_ULA OF ULA IS
+    SIGNAL temp : unsigned(15 DOWNTO 0);
+BEGIN
+    PROCESS (data_in_A, data_in_B, op) BEGIN
+        CASE op IS
+            WHEN "00" =>
+                -- Soma
+                temp <= data_in_A + data_in_B;
+            WHEN "01" =>
+                -- Subtração
+                temp <= data_in_A - data_in_B;
+            WHEN "10" =>
+                -- Comparação igual
+                IF data_in_A = data_in_B THEN
+                    temp <= "0000000000000001";
+                ELSE
+                    temp <= "0000000000000000";
+                END IF;
+            WHEN "11" =>
+                -- Comparação maior
+                IF data_in_A > data_in_B THEN
+                    temp <= "0000000000000001";
+                ELSE
+                    temp <= "0000000000000000";
+                END IF;
+            WHEN OTHERS =>
+                -- Operação inválida
+                temp <= "0000000000000000";
+        END CASE;
+    END PROCESS;
 
-    -- Subtração (utilizando complemento de 2)
-    result_out <= data_in_A + (not data_in_B) + 1;
-
-    -- Comparação (retorna 1 se A >= B, 0 caso contrário)
-    flag_out <= '1' when data_in_A >= data_in_B else '0';
-
-end a_ula;
-
-
+    result_out <= temp(15 DOWNTO 0);
+    zero_out <= '1' WHEN temp(15 DOWNTO 0) = 0 ELSE
+        '0';
+END A_ULA;

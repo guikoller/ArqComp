ef4660c0975798e80f99f27afdca42f022c992ac
guikoller <g_koller@hotmail.com>
2023-04-19 17:03:16 -0300

    fix: register_unit & register_bank

--- register_bank.vhd
+++ register_bank.vhd
@@ -4,39 +4,69 @@ USE ieee.numeric_std.ALL;
 
 ENTITY register_bank IS
     PORT (
-        clk : IN STD_LOGIC;
-        rst : IN STD_LOGIC;
-        write_enable : IN STD_LOGIC;
-        write_data : IN UNSIGNED(15 DOWNTO 0);
-        read_select1 : IN UNSIGNED(2 DOWNTO 0);
-        read_select2 : IN UNSIGNED(2 DOWNTO 0);
-        read_data1 : OUT UNSIGNED(15 DOWNTO 0);
-        read_data2 : OUT UNSIGNED(15 DOWNTO 0)
+        selec_reg_a, selec_reg_b : IN unsigned (2 DOWNTO 0);
+        selec_reg_write : IN unsigned (2 DOWNTO 0);
+        write_data : IN unsigned (15 DOWNTO 0);
+        write_enable, clk, rst : IN STD_LOGIC;
+        reg_data_a, reg_data_b : OUT unsigned (15 DOWNTO 0)
     );
-END register_bank;
+END ENTITY;
 
 ARCHITECTURE a_register_bank OF register_bank IS
-    COMPONENT register_unit -- Componente do registrador
+    COMPONENT register_unit IS
         PORT (
             clk : IN STD_LOGIC;
             rst : IN STD_LOGIC;
             write_enable : IN STD_LOGIC;
             write_data : IN UNSIGNED(15 DOWNTO 0);
-            read_data : OUT UNSIGNED(15 DOWNTO 0)
-        );
+            read_data : OUT UNSIGNED(15 DOWNTO 0));
     END COMPONENT;
-    SIGNAL reg_data_0, reg_data_1, reg_data_2, reg_data_3, reg_data_4, reg_data_5, reg_data_6, reg_data_7: unsigned(15 DOWNTO 0); -- Array de 8 registradores
-BEGIN
-    -- Instanciação dos registradores
-    reg0 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => '0', write_data => (OTHERS => '0'), read_data => reg_data_0);
-    reg1 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_1);
-    reg2 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_2);
-    reg3 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_3);
-    reg4 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_4);
-    reg5 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_5);
-    reg6 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_6);
-    reg7 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable, write_data => write_data, read_data => reg_data_7);
 
-    read_data1 <= reg_data(to_integer(unsigned(read_select1))); -- Saída do valor do registrador selecionado para leitura 1
-    read_data2 <= reg_data(to_integer(unsigned(read_select2))); -- Saída do valor do registrador selecionado para leitura 2
+    SIGNAL read_data_0, read_data_1, read_data_2, read_data_3, read_data_4, read_data_5, read_data_6, read_data_7 : unsigned (15 DOWNTO 0);
+    SIGNAL write_enable_1, write_enable_2, write_enable_3, write_enable_4, write_enable_5, write_enable_6, write_enable_7 : STD_LOGIC;
+
+BEGIN
+    reg0 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => '0', write_data => (OTHERS => '0'), read_data => read_data_0);
+    reg1 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_1, write_data => write_data, read_data => read_data_1);
+    reg2 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_2, write_data => write_data, read_data => read_data_2);
+    reg3 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_3, write_data => write_data, read_data => read_data_3);
+    reg4 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_4, write_data => write_data, read_data => read_data_4);
+    reg5 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_5, write_data => write_data, read_data => read_data_5);
+    reg6 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_6, write_data => write_data, read_data => read_data_6);
+    reg7 : register_unit PORT MAP(clk => clk, rst => rst, write_enable => write_enable_7, write_data => write_data, read_data => read_data_7);
+
+    reg_data_a <=   read_data_0 WHEN selec_reg_a = "000" ELSE
+                    read_data_1 WHEN selec_reg_a = "001" ELSE
+                    read_data_2 WHEN selec_reg_a = "010" ELSE
+                    read_data_3 WHEN selec_reg_a = "011" ELSE
+                    read_data_4 WHEN selec_reg_a = "100" ELSE
+                    read_data_5 WHEN selec_reg_a = "101" ELSE
+                    read_data_6 WHEN selec_reg_a = "110" ELSE
+                    read_data_7 WHEN selec_reg_a = "111" ELSE
+                    "0000000000000000";
+
+    reg_data_b <=   read_data_0 WHEN selec_reg_b = "000" ELSE
+                    read_data_1 WHEN selec_reg_b = "001" ELSE
+                    read_data_2 WHEN selec_reg_b = "010" ELSE
+                    read_data_3 WHEN selec_reg_b = "011" ELSE
+                    read_data_4 WHEN selec_reg_b = "100" ELSE
+                    read_data_5 WHEN selec_reg_b = "101" ELSE
+                    read_data_6 WHEN selec_reg_b = "110" ELSE
+                    read_data_7 WHEN selec_reg_b = "111" ELSE
+                    "0000000000000000";
+
+    write_enable_1 <= write_enable WHEN selec_reg_write = "001" ELSE
+        '0';
+    write_enable_2 <= write_enable WHEN selec_reg_write = "010" ELSE
+        '0';
+    write_enable_3 <= write_enable WHEN selec_reg_write = "011" ELSE
+        '0';
+    write_enable_4 <= write_enable WHEN selec_reg_write = "100" ELSE
+        '0';
+    write_enable_5 <= write_enable WHEN selec_reg_write = "101" ELSE
+        '0';
+    write_enable_6 <= write_enable WHEN selec_reg_write = "110" ELSE
+        '0';
+    write_enable_7 <= write_enable WHEN selec_reg_write = "111" ELSE
+        '0';
 END a_register_bank;
--- register_unit.vhd
+++ register_unit.vhd
@@ -13,17 +13,15 @@ end register_unit;
 architecture a_register_unit of register_unit is
     signal reg_data : unsigned(15 downto 0) := (others => '0'); -- Registrador com valor inicial de 0
 begin
-    process(clk)
+    process(clk, rst, write_enable)
     begin
-        if rising_edge(clk) then
-            if rst = '1' then -- Reset ativo
-                reg_data <= (others => '0'); -- Zera o registrador
-            elsif write_enable = '1' then -- Habilita escrita
+        if rst = '1' then-- Reset ativo
+            reg_data <= (others => '0');-- Zera o registrador
+        elsif write_enable = '1' then   -- Habilita escrita              
+            if rising_edge(clk) then -- Clock
                 reg_data <= write_data; -- Escreve no registrador
             end if;
         end if;
     end process;
-
     read_data <= reg_data; -- Saída do valor do registrador
-
 end a_register_unit;

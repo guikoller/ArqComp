7325eef14aba7240aee0a502d6aba88f13968c97
guikoller <g_koller@hotmail.com>
2023-05-03 16:51:45 -0300

    two states state machine implemented

---
+++ state_machine.vhd
@@ -0,0 +1,24 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY state_machine IS
+    PORT (
+        clk, rst : IN STD_LOGIC;
+        state : OUT STD_LOGIC
+    );
+END state_machine;
+
+ARCHITECTURE a_state_machine OF state_machine IS
+    signal output: STD_LOGIC;
+BEGIN
+    PROCESS (clk, rst) 
+    BEGIN
+        IF rst = '1' THEN
+            output <= '1';
+        ELSIF rising_edge(clk) THEN
+            output <= NOT output;
+        end IF;
+    END PROCESS;
+    state <= output;
+END a_state_machine;
---
+++ state_machine_tb.vhd
@@ -0,0 +1,54 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY state_machine_tb IS
+END state_machine_tb;
+
+ARCHITECTURE a_state_machine_tb OF state_machine_tb IS
+
+    SIGNAL clk,rst : STD_LOGIC := '0';
+    SIGNAL state : STD_LOGIC := '0';
+
+    COMPONENT state_machine
+        PORT (
+            clk, rst : IN STD_LOGIC;
+            state : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+BEGIN
+
+    uut : state_machine
+    PORT MAP (
+        clk => clk,
+        rst => rst,
+        state => state
+    );
+
+    clk_process : PROCESS
+    BEGIN
+        WHILE now < 1000 ns LOOP
+            clk <= '0';
+            WAIT FOR 10 ns;
+            clk <= '1';
+            WAIT FOR 10 ns;
+        END LOOP;
+        WAIT;
+    END PROCESS;
+
+    -- Reset inicial
+    reset_process : PROCESS
+    BEGIN
+        rst <= '1';
+        WAIT FOR 100 ns;
+        rst <= '0';
+        WAIT;
+    END PROCESS;
+
+    test_process : PROCESS
+    BEGIN
+        WAIT;
+    END PROCESS;
+
+END a_state_machine_tb;

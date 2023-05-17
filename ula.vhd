3cc2311285496892d52bdce69361c83a476bb16c
guikoller <g_koller@hotmail.com>
2023-05-16 22:49:20 -0300

    ula with flags

--- ula.vhd
+++ ula.vhd
@@ -6,31 +6,51 @@ ENTITY ULA IS
     PORT (
         data_in_A : IN unsigned(15 DOWNTO 0);
         data_in_B : IN unsigned(15 DOWNTO 0);
-        op : IN unsigned(1 DOWNTO 0);
+        op : IN unsigned(3 DOWNTO 0);
         result_out : OUT unsigned(15 DOWNTO 0);
-        zero_out : OUT STD_LOGIC
+        zero_out, negative_out, carry_out, overflow_out : OUT STD_LOGIC
     );
 END ULA;
 
 ARCHITECTURE A_ULA OF ULA IS
     SIGNAL temp : unsigned(15 DOWNTO 0);
+    SIGNAL negative, carry, overflow : STD_LOGIC;
 BEGIN
     PROCESS (data_in_A, data_in_B, op) BEGIN
         CASE op IS
-            WHEN "00" =>
+            WHEN "0000" =>
                 -- Soma
                 temp <= data_in_A + data_in_B;
-            WHEN "01" =>
+
+                IF data_in_A(15) = '1' AND data_in_B(15) = '1' THEN
+                    carry <= '1';
+                ELSE
+                    carry <= '0';
+                END IF;
+
+                IF (data_in_A + data_in_B) > "1111111111111111" THEN
+                    overflow <= '1';
+                ELSE
+                    overflow <= '0';
+                END IF;
+
+            WHEN "0001" =>
                 -- Subtração
                 temp <= data_in_A - data_in_B;
-            WHEN "10" =>
+
+                IF data_in_A < data_in_B THEN
+                    negative <= '1';
+                ELSE
+                    negative <= '0';
+                END IF;
+            WHEN "0010" =>
                 -- Comparação igual
                 IF data_in_A = data_in_B THEN
                     temp <= "0000000000000001";
                 ELSE
                     temp <= "0000000000000000";
                 END IF;
-            WHEN "11" =>
+            WHEN "0011" =>
                 -- Comparação maior
                 IF data_in_A > data_in_B THEN
                     temp <= "0000000000000001";
@@ -46,4 +66,7 @@ BEGIN
     result_out <= temp(15 DOWNTO 0);
     zero_out <= '1' WHEN temp(15 DOWNTO 0) = 0 ELSE
         '0';
+    carry_out <= carry;
+    overflow_out <= overflow;
+    negative_out <= negative;
 END A_ULA;
--- ula_tb.vhd
+++ ula_tb.vhd
@@ -3,83 +3,74 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY ULA_TB IS
-END;
+END ULA_TB;
 
 ARCHITECTURE A_ULA_TB OF ULA_TB IS
-    COMPONENT ULA IS
+    -- Component declaration for ULA
+    COMPONENT ULA
         PORT (
             data_in_A : IN unsigned(15 DOWNTO 0);
             data_in_B : IN unsigned(15 DOWNTO 0);
-            op : IN unsigned(1 DOWNTO 0);
+            op : IN unsigned(3 DOWNTO 0);
             result_out : OUT unsigned(15 DOWNTO 0);
-            zero_out : OUT STD_LOGIC
+            zero_out, negative_out, carry_out, overflow_out : OUT STD_LOGIC
         );
     END COMPONENT;
-    SIGNAL data_in_A, data_in_B, result_out : unsigned(15 DOWNTO 0);
-    SIGNAL op : unsigned(1 DOWNTO 0);
-    SIGNAL zero_out : STD_LOGIC;
+
+    -- Testbench signals
+    SIGNAL data_A, data_B : unsigned(15 DOWNTO 0);
+    SIGNAL operation : unsigned(3 DOWNTO 0);
+    SIGNAL result : unsigned(15 DOWNTO 0);
+    SIGNAL zero, negative, carry, overflow : STD_LOGIC;
+
 BEGIN
-    uut : ULA PORT MAP(
-        data_in_A => data_in_A,
-        data_in_B => data_in_B,
-        op => op,
-        result_out => result_out,
-        zero_out => zero_out
+    -- Instantiate the ULA component
+    uut : ULA
+    PORT MAP(
+        data_in_A => data_A,
+        data_in_B => data_B,
+        op => operation,
+        result_out => result,
+        zero_out => zero,
+        negative_out => negative,
+        carry_out => carry,
+        overflow_out => overflow
     );
+
+    -- Stimulus process
     PROCESS
     BEGIN
-        -- Teste com soma
-        op <= "00";
-        data_in_A <= x"0005";
-        data_in_B <= x"0002";
+        -- Testcase 1: Addition
+        data_A <= "1000000000000010";
+        data_B <= "1000000000000011";
+        operation <= "0000";
+        WAIT FOR 10 ns;
+
+        -- Testcase 2: Subtraction
+        data_A <= "0000000000000011";
+        data_B <= "0000000000000010";
+        operation <= "0001";
+        WAIT FOR 10 ns;
+
+        -- Testcase 3: Comparison (Equal)
+        data_A <= "0000000000000010";
+        data_B <= "0100000000000010";
+        operation <= "0010";
+        WAIT FOR 10 ns;
+
+        -- Testcase 4: Comparison (Greater)
+        data_A <= "0000000000000100";
+        data_B <= "0000000000000010";
+        operation <= "0011";
+        WAIT FOR 10 ns;
+
+        -- Testcase 5: Invalid operation
+        data_A <= "0000000000000001";
+        data_B <= "0000000000000001";
+        operation <= "0100";
         WAIT FOR 10 ns;
         
-        data_in_A <= x"0010";
-        data_in_B <= x"0050";
-        WAIT FOR 10 ns;
-        
-        data_in_A <= x"1010";
-        data_in_B <= x"0401";
-        WAIT FOR 10 ns;
-
-        -- Teste com subtração
-        op <= "01";
-        data_in_A <= x"0005";
-        data_in_B <= x"0002";
-        WAIT FOR 10 ns;
-        
-        data_in_A <= x"1000";
-        data_in_B <= x"0500";
-        WAIT FOR 10 ns;
-        
-        data_in_A <= x"0007";
-        data_in_B <= x"0001";
-        WAIT FOR 10 ns;
-
-        -- Teste com comparação igual
-        op <= "10";
-        data_in_A <= x"0005";
-        data_in_B <= x"0005";
-        WAIT FOR 10 ns;
-        
-        data_in_A <= x"0011";
-        data_in_B <= x"0005";
-        WAIT FOR 10 ns;
-
-        -- Teste com comparação maior
-        op <= "11";
-        data_in_A <= x"0005";
-        data_in_B <= x"0002";
-        WAIT FOR 10 ns;
-
-        data_in_A <= x"0002";
-        data_in_B <= x"0005";
-        WAIT FOR 10 ns;
-
-        data_in_A <= x"0010";
-        data_in_B <= x"0002";
-        WAIT FOR 10 ns;
-
         WAIT;
     END PROCESS;
+
 END A_ULA_TB;

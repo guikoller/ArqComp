61fcc8bb4f57b52f0bcdaf8776d3f344c71e56af
guikoller <g_koller@hotmail.com>
2023-06-21 20:24:00 -0300

    SIEVEEEEEEEE

--- sieve.c
+++ sieve.c
@@ -1,31 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int n = 100;
-	int prime[n+1];
-	//Loading the array with numbers from 1 to n
-	for(int i = 1; i <= n; i++)
-	{
+	int n = 32;
+	int prime[n + 1];
+
+	for (int i = 1; i <= n; i++)
 		prime[i] = i;
-	}
-	//Start with least prime number, which is 2.
-	//No need to check for numbers greater than square root of n.
-	//They will be already marked.
-	for(int i = 2; i*i <= n; i++)
-	{
-		if(prime[i] != -1)
-		{
-			//Mark all the multiples of i as -1.
-			for(int j = 2*i; j <=n ; j += i)
-				prime[j] = -1;
-		}
-	}
+
+	for (int i = 4; i <= n; i = i + 2)
+		prime[i] = -1;
+
+	for (int i = 6; i <= n; i = i + 3)
+		prime[i] = -1;
+
+	for (int i = 10; i <= n; i = i + 5)
+		prime[i] = -1;
+
 	printf("Prime numbers are: \n");
-	for(int i=2; i <= n; i++)
-	{
-		if(prime[i] != -1)
-		{
+	for (int i = 2; i <= n; i++){
+		if (prime[i] != -1)
 			printf("%d ", i);
-		}
 	}
 }
